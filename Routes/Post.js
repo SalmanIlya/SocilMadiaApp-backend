e52daf82b1526f7ea6413aa913e08873b2e9caa3
Salamanilyas <salamanilyas101@gmail.com>
2023-11-02 12:48:57 +0500

    post-routes

--- Routes/Post.js
+++ Routes/Post.js
@@ -1,7 +1,11 @@
 const express=require("express")
+const { createPost, UpdatePost, DeletePost, getMyPosts } = require("../Controllers/Post")
 const app=express.Router()
 
-
+app.post("/create/:id",createPost)
+app.put("/update/:id/:porductId",UpdatePost)
+app.delete("/delete/:id/:porductId",DeletePost)
+app.post("/getmypost/:id",getMyPosts)
 
 
 module.exports=app
