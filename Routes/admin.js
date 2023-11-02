401b3f08a08887a2d5ea20457c31b86fb4c0f3c4
Salamanilyas <salamanilyas101@gmail.com>
2023-11-02 12:48:13 +0500

    small-updates

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,8 +1,9 @@
 const express=require("express")
 const user=require("./User")
+const Post = require("./Post")
 const app=express.Router()
 
-app.use("/admin",user)
-
+app.use("/user",user)
+app.use("/post",Post)
 
 module.exports=app
